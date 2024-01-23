fcee8b30aeae60fbf742f710950415cfdf886ed6
harshilmadaliya <harshilmadaliyag@gmail.com>
2024-01-23 19:43:33 +0530

    favicon changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-'use client'
-import { signOut } from 'next-auth/react'
+import { NotebookPen } from "lucide-react";
+import Head from "next/head";
+import React from "react";
 
 export default function Home() {
   
   return (
     <>
+    <Head>
+      <NotebookPen  />
+    </Head>
     <div className='bg-red-400'>Hello</div>
     </>
   )
